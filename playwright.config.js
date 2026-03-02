ca1014cd6607165c228d51feabc1a24f2c2ea51b
Drazen Gajic <d.gajic@vega.local>
2026-03-02 16:07:34 +0100

    Addition of the monocartReporter for .pdf reports

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,13 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'], // Shows progress in terminal
+    ['monocart-reporter', {  
+        name: "OrangeHRM Automation Report",
+        outputFile: './test-results/report.html'
+    }]
+],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
    /* Base URL for the project */
